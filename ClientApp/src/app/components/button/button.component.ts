a609501af1d07367629cfae8ff9b315e35099008
Raul Rohjans <rauljann@hotmail.com>
2025-01-11 20:39:14 +0000

    Added Home page and button component as a test

---
+++ ClientApp/src/app/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  btnClick() {
+    
+    alert("Sent!")
+  }
+}
