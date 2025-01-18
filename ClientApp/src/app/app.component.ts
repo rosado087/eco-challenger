f141549c7c62584539608e41b1996450473011c0
Raul Rohjans <rauljann@hotmail.com>
2025-01-18 19:22:57 +0000

    Added app routing and removed trash from the homepage

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { HomeComponent } from './pages/home/home.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HomeComponent],
+    imports: [RouterOutlet, HeaderComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
-    title = 'ClientApp'
+    
 }
