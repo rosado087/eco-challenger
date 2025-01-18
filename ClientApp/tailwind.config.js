2679b15e2ff128a56887ae2863042f1be54514cc
Raul Rohjans <rauljann@hotmail.com>
2025-01-18 19:22:36 +0000

    Overwritten some tailwind colours

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: ":root", // The element that receives theme color CSS variables
+
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#78a47c",
+          secondary: "#b1ffc1"
+        }
+      }
+    ]
   },
 }
 
