c153d93eaa44e486d21fdf829fb5de3d71facc6f
Diogo <diogosiva.silva@gmail.com>
2025-02-04 22:09:25 +0000

    Adds the new user with a hashed password

--- EcoChallenger/Controllers/RegisterController.cs
+++ EcoChallenger/Controllers/RegisterController.cs
@@ -15,6 +15,14 @@ namespace EcoChallenger.Controllers
             _ctx = context;
         }
 
+        /// <summary>
+        /// Handles the action that creates a new User.
+        /// Verifies if the email which the User wrote is not being used already.
+        /// If not, creates a new hashed password.
+        /// Add the new User into the database.
+        /// </summary>
+        /// <param name="data">Contains the user's username, email address and password</param>
+        /// <returns>JSON result indicating success or failure. If returns failure also returns the error message</returns>
 
         [HttpPost("RegisterAccount")]
         public async Task<JsonResult> RegisterAccount([FromBody] User data)  {
@@ -26,10 +34,15 @@ namespace EcoChallenger.Controllers
                 if (emailExists)
                     return new JsonResult(new { success = false, message = "Este email já existe" });
 
-                // Adiciona um novo usuário
+
+                //userTkn.User.Password = PasswordGenerator.GeneratePasswordHash(data.Password);
+
+                data.Password = PasswordGenerator.GeneratePasswordHash(data.Password);
+
+                // Adiciona um novo utilizador
                 await _ctx.Users.AddAsync(data);
                 await _ctx.SaveChangesAsync();
-
+                // retorna uma resposta JSON com sucessor true
                 return new JsonResult(new { success = true });
             }
             catch (Exception e)
