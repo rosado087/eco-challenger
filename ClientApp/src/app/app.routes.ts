3b71c532100c86d4d07dbe3ecfdc9dab3e7d53d4
rosado087 <201902087@estudantes.ips.pt>
2025-03-29 17:14:27 +0000

    Added the component for the challenges frontend, no connection to the backend part yet

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -59,6 +59,15 @@ export const routes: Routes = [
         canActivate: [authGuard]
     },
     {
+        path: 'challenges/:id',
+        loadComponent: () => {
+            return import('./pages/challenges/challenges.component').then(
+                (m) => m.ChallengesComponent
+            )
+        },
+        canActivate: [authGuard]
+    },
+    {
         path: 'store',
         loadComponent: () => {
             return import('./pages/store/store.component').then(
