f1320083da1eba55d85708997beb1d4f9016f95d
Diogo <diogosiva.silva@gmail.com>
2025-03-30 22:03:17 +0100

    Complete and add progress fully implemented

--- ClientApp/src/app/pages/challenges/challenges.component.ts
+++ ClientApp/src/app/pages/challenges/challenges.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../../services/auth/auth.service'
 import { ChallengeListModel } from '../../models/gamification-model';
 import { ChallengeCardComponent } from '../../components/challenge-card/challenge-card.component';
 import { ChallengeModel } from '../../models/challenge-model';
+import { SuccessModel } from '../../models/success-model';
 
 @Component({
   selector: 'app-challenges',
@@ -19,69 +20,94 @@ import { ChallengeModel } from '../../models/challenge-model';
       ]
 })
 export class ChallengesComponent implements OnInit {
-  netApi = inject(NetApiService)
-  popupLoader = inject(PopupLoaderService)
-  router = inject(Router)
-  authService = inject(AuthService)
-  dailyChallenges: { challenge: ChallengeModel, wasConcluded: boolean }[] = []
-  weeklyChallenges: {challenge: ChallengeModel, progress: number, wasConcluded: boolean}[] = []
-  selectedChallenge: ChallengeModel | null = null
+    netApi = inject(NetApiService)
+    popupLoader = inject(PopupLoaderService)
+    router = inject(Router)
+    authService = inject(AuthService)
+    dailyChallenges: { challenge: ChallengeModel, wasConcluded: boolean }[] = []
+    weeklyChallenges: {challenge: ChallengeModel, progress: number, wasConcluded: boolean}[] = []
+    selectedChallenge: ChallengeModel | null = null
 
-  ngOnInit(): void {
-    this.getChallenges()
-    
-  }
+    ngOnInit(): void {
+        this.getChallenges()
+    }
 
-  getChallenges() {
-      this.netApi
-          .get<ChallengeListModel>('Gamification', 'GetChallenges')
-          .subscribe({
-                      next: (data) => {
-                          if (data.success) {
-                             this.dailyChallenges = data.dailyChallenges
-                             this.weeklyChallenges = data.weeklyChallenges
-                          } else {
-                              this.popupLoader.showPopup(
-                                  'Erro',
-                                  data.message || 'Ocorreu um erro!'
-                              )
-                          }
-                      },
-                      error: () => {
-                          this.popupLoader.showPopup(
-                              'Erro',
-                              'Não foi possível carregar os dados do perfil.'
-                          )
-                      }
-                  })
-  }
-
-  completeChallenge(id: number) {
-    console.log(id);
-    this.netApi
-        .post<ChallengeListModel>('Gamification', 'CompleteChallenge', id)
+    getChallenges() {
+        this.netApi
+        .get<ChallengeListModel>('Gamification', 'GetChallenges')
         .subscribe({
-                    next: (data) => {
-                        if (data.success) {
-                          this.dailyChallenges = data.dailyChallenges
-                          this.weeklyChallenges = data.weeklyChallenges
-                          this.popupLoader.showPopup(
-                            'Sucesso',
-                            data.message || 'Concluído com sucesso'
-                        )
-                        } else {
-                            this.popupLoader.showPopup(
-                                'Erro',
-                                data.message || 'Ocorreu um erro!'
-                            )
-                        }
-                    },
-                    error: () => {
-                        this.popupLoader.showPopup(
-                            'Erro',
-                            'Não foi possível concluir o desafio.'
-                        )
-                    }
-                })
-  }
+            next: (data) => {
+                if (data.success) {
+                    this.dailyChallenges = data.dailyChallenges
+                    this.weeklyChallenges = data.weeklyChallenges
+                } else {
+                    this.popupLoader.showPopup(
+                        'Erro',
+                        data.message || 'Ocorreu um erro!'
+                    )
+                }
+            },
+            error: () => {
+                this.popupLoader.showPopup(
+                    'Erro',
+                    'Não foi possível carregar os dados do perfil.'
+                )
+            }
+        })
+    }
+
+    completeChallenge (id: number) {
+        this.netApi
+        .get<SuccessModel>('Gamification', 'CompleteChallenge', undefined, id.toString())
+        .subscribe({
+            next: (data) => {
+                if (data.success) {
+                    this.popupLoader.showPopup(
+                        'Sucesso',
+                        data.message || 'Concluído com sucesso'
+                    )
+                    this.getChallenges()
+                } else {
+                    this.popupLoader.showPopup(
+                        'Erro',
+                        data.message || 'Ocorreu um erro!'
+                    )
+                }
+            },
+            error: () => {
+                this.popupLoader.showPopup(
+                    'Erro',
+                    'Não foi possível concluir o desafio.'
+                )
+            }
+        })
+    }
+
+    addProgress (id: number) {
+        this.netApi
+        .get<SuccessModel>('Gamification', 'AddProgress', undefined, id.toString())
+        .subscribe({
+            next: (data) => {
+                if (data.success) {
+                    this.popupLoader.showPopup(
+                        'Sucesso',
+                        data.message || 'Progresso adicionado com sucesso!'
+                    )
+                    this.getChallenges()
+                } else {
+                    this.popupLoader.showPopup(
+                        'Erro',
+                        data.message || 'Ocorreu um erro!'
+                    )
+                }
+            },
+            error: () => {
+                this.popupLoader.showPopup(
+                    'Erro',
+                    'Não foi possível adicionar progresso ao desafio.'
+                )
+            }
+        })
+    }
+
 }
